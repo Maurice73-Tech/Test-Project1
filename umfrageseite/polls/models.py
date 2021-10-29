15e687a024455855cc26b99e41b610f95b5ca331
maurice neidhart <maurice.neidhart97@gmail.com>
2021-10-29 22:01:39 +0200

    latest changes

--- umfrageseite/polls/models.py
+++ umfrageseite/polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField()
+
+class Choice(models.Model):
+    poll = models.ForeignKey(to='Poll', on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    votes = models.IntegerField(default=0)
+    notes = models.TextField(null=True, blank = True)
