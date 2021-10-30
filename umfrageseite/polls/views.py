8dcc10c9fc62322dade55beb7c29d4528564a304
maurice neidhart <maurice.neidhart97@gmail.com>
2021-10-30 17:51:14 +0200

    HTML hinzugefügt

--- umfrageseite/polls/views.py
+++ umfrageseite/polls/views.py
@@ -5,11 +5,4 @@ from django.http import HttpResponse
 from .models import Poll
 
 def index(request):
-    serverantwort = ""
-    for umfrage in Poll.objects.all():
-        serverantwort = serverantwort + "<br />"
-        serverantwort += umfrage.name + " ("
-        for antwortwortmoeglichkeit in umfrage.choice_set.all():
-            serverantwort += antwortwortmoeglichkeit.name + " ,"
-        serverantwort += ")"
-    return HttpResponse(serverantwort)
+    return render(request=request, template_name='polls/index.html')
