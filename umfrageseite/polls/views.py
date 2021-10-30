5eca1c13ae6fbc4784f53b8b5036eec827fc57ec
maurice neidhart <maurice.neidhart97@gmail.com>
2021-10-30 16:58:55 +0200

    new commits

--- umfrageseite/polls/views.py
+++ umfrageseite/polls/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Poll
+
 def index(request):
+    antwort = ""
+    for umfrage in Poll.objects.all():
+        antwort = antwort + " " + umfrage.name
     return HttpResponse("Hallo Welt!")
