a8131b5d32b5058faeb889ad1301a21445bd6925
maurice neidhart <maurice.neidhart97@gmail.com>
2021-10-30 17:03:13 +0200

    new stuff

--- umfrageseite/polls/urls.py
+++ umfrageseite/polls/urls.py
@@ -5,5 +5,5 @@ from .views import index
 urlpattern = [
     # hey, wenn eine Anfrage an reinkommt, dann überge dass der Funktion 
     # index aus der view.py
-    path('', index)
+    path('', index),
 ]
