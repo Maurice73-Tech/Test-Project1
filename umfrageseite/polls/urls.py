46d7ea2c500ea1ee197285e20bdbfed669b5b406
maurice neidhart <maurice.neidhart97@gmail.com>
2021-10-30 17:19:28 +0200

    new changes

--- umfrageseite/polls/urls.py
+++ umfrageseite/polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import index
 
+app_name = 'polls'
 urlpattern = [
     # hey, wenn eine Anfrage an reinkommt, dann überge dass der Funktion 
     # index aus der view.py
