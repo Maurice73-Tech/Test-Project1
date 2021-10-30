ad2b2a7467d0b363daf8554ab002d185974f35c6
maurice neidhart <maurice.neidhart97@gmail.com>
2021-10-30 17:42:35 +0200

    fixed typo

--- umfrageseite/polls/urls.py
+++ umfrageseite/polls/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import index
 
 app_name = 'polls'
-urlpattern = [
+urlpatterns = [
     # hey, wenn eine Anfrage an reinkommt, dann überge dass der Funktion 
     # index aus der view.py
     path('', index),
